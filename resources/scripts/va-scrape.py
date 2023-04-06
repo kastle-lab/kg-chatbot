6a66218a80bc7b1eca7da4df7f4ea9d1e9753df3
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2023-04-06 14:48:13 -0400

    Got va scraping script to work and generated dataset

--- resources/scripts/va-scrape.py
+++ resources/scripts/va-scrape.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from string import ascii_uppercase
 from bs4 import BeautifulSoup
 import pandas as pd
 
-driver = webdriver.Chrome("/Users/anmolsaini/chromedriver_mac64/chromedriver")
-#games_links = []
-#data_df = pd.DataFrame(columns=['Game', 'Character', 'Voice Actor'])
 games_info = []
-for letter in ascii_uppercase:
+
+def scrape_page(driver, letter):
     url = f"https://www.behindthevoiceactors.com/video-games/{letter}/"
+    #url = f"https://www.behindthevoiceactors.com/video-games/$/"
     driver.get(url)
     #for
     letter_page = driver.page_source
@@ -26,7 +26,9 @@ for letter in ascii_uppercase:
         #game_info.append(game_name["title"])
         game_link = game_details_html.find('a', href=True)
         # games_links.append(games_link["href"])
-        driver.get(game_link["href"])
+        #print(f"about to get {game_name['title']} page")
+        driver.get(game_link["href"]) # HANGS UP HERE
+        #print(f"on {game_name['title']} page")
         game_page = driver.page_source
         game_soup = BeautifulSoup(game_page, 'lxml')
         #print(game_soup.prettify())
@@ -44,17 +46,33 @@ for letter in ascii_uppercase:
             #print(game_actor_character_html, end="\n"*2)
             character_name = game_actor_character_html.find('a', title=True)
             #print(names[1]["title"])
-            va_name = character_name.find_next_sibling('a', title=True)
-            if (va_name == None):
-                print(f"{game_name['title']}, {character_name['title']} has no va")
-                continue
             game_info.append(character_name["title"])
-            game_info.append(va_name["title"])
+            #print(f"added character {character_name['title']} from {game_name['title']}")
+            va_name = character_name.find_next_sibling('a', title=True)
+            if (va_name != None):
+                # print(f"{game_name['title']}, {character_name['title']} has no va")
+                # continue
+                game_info.append(va_name["title"])
+                #print(f"added va {va_name['title']} from {game_name['title']}")
             #print(game_info, end="\n")
             games_info.append(game_info)
         # print(games_info, end="\n")
         # print("\n")
     #print(games_links)
+
+chop = webdriver.ChromeOptions()
+chop.add_extension("/Users/anmolsaini/extension_2_5_9_0.crx")
+driver = webdriver.Chrome("/Users/anmolsaini/chromedriver_mac64/chromedriver", chrome_options = chop)
+#games_links = []
+#data_df = pd.DataFrame(columns=['Game', 'Character', 'Voice Actor'])
+
+scrape_page(driver, "$")
+# print(games_info)
+# print("\n\n")
+for letter in ascii_uppercase:
+    scrape_page(driver, letter)
+    # print(games_info)
+    
 data_df = pd.DataFrame(games_info, columns = ['Game', 'Character', 'Voice Actor'])
 data_df.to_csv('va-dataset.csv', index=False)
     #exit()
