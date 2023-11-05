49073f47d14949e96dd8c88923ed03d7fb5687eb
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2023-11-05 15:02:11 -0500

    Some more NER testing
    Spacy with trf model seems ideal

--- resources/scripts/NLP/NER-test-nltk.py
+++ resources/scripts/NLP/NER-test-nltk.py
@@ -35,15 +35,15 @@ from nltk import ne_chunk
 # from wordcloud import WordCloud
 
 #pos_ner_text = "Bill Gates founded Microsoft Corp. together with Paul Allen in 1975."
-# pos_ner_text = "European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices"
-#pos_ner_text = "Jane Austen wrote Pride and Prejudice"
-#pos_ner_text = "Mount Everest is the tallest mountain in the world."
+#pos_ner_text = "European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices."
+#pos_ner_text = "Jane Austen wrote Pride and Prejudice."
+pos_ner_text = "Mount Everest is the tallest mountain in the world."
 
-# pos_ner_text = "Gearbox Software developed Borderlands and released it in 2009. It sold 600,000 units in Europe."
+#pos_ner_text = "Gearbox Software developed Borderlands and released it in 2009. It sold 600,000 units in Europe."
 #pos_ner_text = "In The Legend of Zelda: Skyward Sword, Ghirahim seeks to revive the Demon King Demise."
 #pos_ner_text = "Sonic the Hedgehog is said to be as fast as the speed of sound, approximately 343 meters per second."
 #pos_ner_text = "Metal Gear Solid takes place in an alternate history in which the Cold War continued into the 1990s, ending at some point near the end of the 20th century."
-pos_ner_text = "In The Legend of Zelda: Breath of the Wild, Link meets King Rhoam Bosphoramus Hyrule after awaking from a century of slumber in the Shrine of Resurrection."
+#pos_ner_text = "In The Legend of Zelda: Breath of the Wild, Link meets King Rhoam Bosphoramus Hyrule after awaking from a century of slumber in the Shrine of Resurrection."
 
 #POS_tag = pos_tag(word_tokenize(pos_ner_text))
 NER_tree = ne_chunk(pos_tag(word_tokenize(pos_ner_text)))
--- resources/scripts/NLP/NER-test-spacy.py
+++ resources/scripts/NLP/NER-test-spacy.py
@@ -1,20 +1,26 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
 from pprint import pprint
 import spacy
 from spacy import displacy
 #from collections import Counter
-import en_core_web_sm
-nlp = en_core_web_sm.load()
+import en_core_web_trf
+nlp = en_core_web_trf.load()
 
 #doc = nlp("Bill Gates founded Microsoft Corp. together with Paul Allen in 1975.")
-#doc = nlp('European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices')
-#doc = nlp("Jane Austen wrote Pride and Prejudice")
+#doc = nlp('European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices.')
+#doc = nlp("Jane Austen wrote Pride and Prejudice.")
 #doc = nlp("Mount Everest is the tallest mountain in the world.")
 
-# doc = nlp("Gearbox Software developed Borderlands and released it in 2009. It sold 600,000 units in Europe.")
+#doc = nlp("Gearbox Software developed Borderlands and released it in 2009. It sold 600,000 units in Europe.")
 #doc = nlp("In The Legend of Zelda: Skyward Sword, Ghirahim seeks to revive the Demon King, Demise.")
 #doc = nlp("Sonic the Hedgehog is said to be as fast as the speed of sound, approximately 343 meters per second.")
 #doc = nlp("Metal Gear Solid takes place in an alternate history in which the Cold War continued into the 1990s, ending at some point near the end of the 20th century.")
-doc = nlp("In The Legend of Zelda: Breath of the Wild, Link meets King Rhoam Bosphoramus Hyrule after awaking from a century of slumber in the Shrine of Resurrection.")
+#doc = nlp("In The Legend of Zelda: Breath of the Wild, Link meets King Rhoam Bosphoramus Hyrule after awaking from a century of slumber in the Shrine of Resurrection.")
+#doc = nlp("Street Fighter 6 is a fighting game developed by Capcom.")
+#doc = nlp("Xenoblade Chronicles is a JRPG developed by Monolith Soft and published by Nintendo in 2008.")
+doc = nlp("Chrom from Fire Emblem Awakening is voiced by Matt Mercer. He wield the sword Falchion.")
 
 pprint([(X.text, X.label_) for X in doc.ents])
 
