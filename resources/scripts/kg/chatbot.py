2448d05ffbc930e4dcf02752c8c8903051c189b0
L30N1DAS <77419369+L30N1DAS@users.noreply.github.com>
2023-11-30 22:58:11 -0500

    Delete resources/scripts/kg/chatbot.py
    Deleting old chatbot code

--- resources/scripts/kg/chatbot.py
+++
@@ -1,5 +0,0 @@
-question = input("Bot: Hi, I am a knowledge graph-powered chatbot focusing on information about video games. What would you like to know?\nYou: ")
-
-while True:
-    print(f"Bot: {question} That's a great question!")
-    question = input("You: ")
